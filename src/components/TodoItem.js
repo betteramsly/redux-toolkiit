04c6e87e1dad001d0410166fa3aa7e581dc38fa0
Amin Beterbekow <betteramsly>
2022-08-24 16:26:48 +0300

    реализовал: добавление, изменение, удаление

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,20 +1,40 @@
-import React from 'react'
+import React from "react";
+import { useDispatch } from "react-redux";
+import { removeTodo, toggleCompletedTodo } from "../features/todo/todoSlice";
 
-const TodoItem = () => {
-    const completed = false
-    return (
-        <div className='flex justify-between items-center my-2'>
-            <div className='text-sm px-4 py-2 cursor-pointer bg-lime-300 hover:bg-lime-400'>
-                Complete
-            </div>
-            <div className={`text-sm ${completed ? 'line-through font-medium text-lime-400' : ''}`}>
-                Todo Text
-            </div>
-            <div className='text-sm px-4 py-2 flex bg-red-400 hover:bg-red-500 transition-all text-white cursor-pointer'>
-                Delete
-            </div>
-        </div>
-    )
-}
+const TodoItem = ({ todo }) => {
+  const dispatch = useDispatch();
 
-export default TodoItem
+  const toggleTodoHandler = (id) => {
+    dispatch(toggleCompletedTodo(id));
+  };
+
+  const removeTodoHadler = (id) => {
+    dispatch(removeTodo(id));
+  };
+  return (
+    <div className="flex justify-between items-center my-2">
+      <div
+        onClick={() => toggleTodoHandler(todo.id)}
+        className="text-sm px-4 py-2 cursor-pointer bg-lime-300 hover:bg-lime-400"
+      >
+        Complete
+      </div>
+      <div
+        className={`text-sm ${
+          todo.completed ? "line-through font-medium text-lime-400" : ""
+        }`}
+      >
+        {todo.text}
+      </div>
+      <div
+        onClick={() => removeTodoHadler(todo.id)}
+        className="text-sm px-4 py-2 flex bg-red-400 hover:bg-red-500 transition-all text-white cursor-pointer"
+      >
+        Delete
+      </div>
+    </div>
+  );
+};
+
+export default TodoItem;
